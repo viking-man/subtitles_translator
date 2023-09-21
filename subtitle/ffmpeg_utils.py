937ea5571d63a6c5d17b678c848c56acef3bf280
viking <jiangwei4953267@163.com>
2023-09-21 18:01:10 +0800

    1.ffmpeg path问题

--- subtitle/ffmpeg_utils.py
+++ subtitle/ffmpeg_utils.py
@@ -1,12 +1,27 @@
 import logging
 import ffmpeg
-from pathlib import Path
+import os
+import re
+import sys
+
+
+def escape_windows_path(path):
+    # 将单个反斜杠替换为双反斜杠
+    return re.sub(r'\\', r'\\\\', path)
+
+
+def is_windows():
+    return sys.platform.startswith('win')
 
 
 def add_subtitles(video_file, subtitle_file, output_file):
     # 使用 ffmpeg.input() 来指定输入文件和字幕文件
-    input_video = ffmpeg.input(Path(video_file))
-    input_subtitle = ffmpeg.input(Path(subtitle_file))
+    input_video = ffmpeg.input(video_file)
+    input_subtitle = ffmpeg.input(subtitle_file)
+
+    # 区分系统
+    if is_windows():
+        subtitle_file = escape_windows_path(subtitle_file)
 
     # 使用 filter() 添加字幕
     output = ffmpeg.output(
@@ -17,7 +32,7 @@ def add_subtitles(video_file, subtitle_file, output_file):
         acodec='copy',  # 音频编解码器，此处保持原样
         scodec='mov_text',  # 字幕编解码器
         f='mp4',  # 输出文件格式
-        vf=f'subtitles={Path(subtitle_file)}',  # 添加字幕滤镜
+        vf=f'subtitles={subtitle_file}',  # 添加字幕滤镜
         strict='experimental',  # 使用实验性字幕编解码器
     )
 
